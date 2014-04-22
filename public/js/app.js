1967e4708b1bb36843bf88e3da46e18654a2a140
Bondifrench <d_dumaine@yahoo.com>
2014-04-22 10:57:33 +1000

    adding nested views

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,17 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, 
 			url: '/home',
 			templateUrl:'./partials/partial-home.html'
 			})
+		.state('home.list',{
+			url:'/list',
+			templateUrl:'./partials/partial-home-list.html',
+			controller: function($scope){
+				$scope.dogs=['Bernese', 'Husky', 'Goldendoodle'];
+			}
+		})
+		.state('home.paragraph', {
+			url: '/paragraph',
+			template:'Some random text for illutrative purposes'
+		})
 		.state('about', {
 			url: '/about',
 			templateUrl:'./partials/partial-about.html'
